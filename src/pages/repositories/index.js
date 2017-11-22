3e7d15d64b6e3b4ade18aa82496939f04aded570
Rodrigo Rosa <rodrigocrosa@gmail.com>
2017-11-22 21:25:19 -0200

    Routes

---
+++ src/pages/repositories/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class Repositories extends Component {
+  render() {
+    return (
+      <View />
+    );
+  }
+}
