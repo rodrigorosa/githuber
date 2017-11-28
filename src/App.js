5fe4b092c65b50bfe3486975c3f6dcc54fdaa81d
Rodrigo Rosa <rodrigocrosa@gmail.com>
2017-11-28 20:45:22 -0200

    Initial route based on user on AsyncStorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import 'config/ReactotronConfig';
-import Routes from 'routes';
+import createRouteNavigator from 'routes';
+import { AsyncStorage } from 'react-native';
+
 
 export default class App extends Component {
+  state = {
+    userExists: false,
+    userChecked: false,
+  };
+
+  componentWillMount() {
+    // AsyncStorage.clear();
+    this.checkUser().then((response) => {
+      this.setState({ userExists: response, userChecked: true });
+    });
+  }
+
+  checkUser = async () => {
+    const user = await AsyncStorage.getItem('@Githuber:username');
+    return user !== null;
+  };
+
   render() {
-    return (
-      <Routes />
-    );
+    const { userExists, userChecked } = this.state;
+
+    if (!userChecked) return null;
+
+    const Layout = createRouteNavigator(userExists);
+    return <Layout />;
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,17 @@ import Welcome from 'pages/welcome';
 import Repositories from 'pages/repositories';
 import Organizations from 'pages/organizations';
 
-const Routes = StackNavigator({
-  Welcome: { screen: Welcome },
-  User: {
-    screen: TabNavigator({  
-      Repositories: { screen: Repositories },
-      Organizations: { screen: Organizations },
-    }),
-  },
-}, {
-  initialRouteName: 'Welcome',
+const createRouteNavigator = (userExists = false) =>
+  StackNavigator({
+    Welcome: { screen: Welcome },
+    User: {
+      screen: TabNavigator({  
+        Repositories: { screen: Repositories },
+        Organizations: { screen: Organizations },
+      }),
+    },
+  }, {
+    initialRouteName: userExists ? 'User' : 'Welcome',
 });
 
-export default Routes;
+export default createRouteNavigator;
