22606c30c199db02c71b43b7464984715490e589
Rodrigo Rosa <rodrigocrosa@gmail.com>
2017-11-27 22:46:29 -0200

    Redirecting user

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { NavigationActions } from 'react-navigation';
+import PropTypes from 'prop-types';
+
 import styles from './styles';
 
 export default class Welcome extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      dispatch: PropTypes.func,
+    }).isRequired,
+  }
+
   static navigationOptions = {
     header: null,
+  }
+
+  navigateToUser = () => {
+    const { dispatch } = this.props.navigation;
+
+    const resetAction = NavigationActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({ routeName: 'User' }),
+      ],
+    });
+
+    dispatch(resetAction);
   }
 
   render() {
@@ -14,13 +36,13 @@ export default class Welcome extends Component {
         <Text style={styles.welcomeDescription}>
           Para continuar, precisamos que você informe o seu usuário no Github.
         </Text>
-        
-        <TextInput 
+
+        <TextInput
           style={styles.input}
           placeholder="Digite o seu usuário" 
         />
 
-        <TouchableOpacity style={styles.button} onPress={ () => {} }>
+        <TouchableOpacity style={styles.button} onPress={ this.navigateToUser }>
           <Text style={styles.buttonText}>Prosseguir</Text>
         </TouchableOpacity>
       </View>
