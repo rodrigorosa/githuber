708f453755398011077d0c5861ce8d36c87ee54d
Rodrigo Rosa <rodrigo@wonder.com.br>
2017-12-01 16:23:01 -0200

    Loading repositories from remote api

--- src/pages/repositories/index.js
+++ src/pages/repositories/index.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { View, Text, AsyncStorage, ActivityIndicator } from 'react-native';
+import Repository from './components/repository';
+import api from 'services/api';
+
+import styles from './styles';
 
 export default class Repositories extends Component {
-  static navigationOptions = {
-    tabBarIcon: ({ tintColor }) => (
-      <Icon name="list-alt" size={20} color={tintColor} />
-    ),
+  state = {
+    repositories: [],
+    loading: false,
+  };
+
+  componentWillMount() {
+    this.loadRepositories();
   }
+
+  loadRepositories = async () => {
+    console.tron.log('loading repositories');
+    this.setState({ loading: true });
+
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const response = await api.get(`/users/${username}/repos`);
+
+    console.tron.log(`username: ${username}`);
+    console.tron.log(response.data);
+
+    this.setState({ repositories: response.data, loading: false });
+  };
+
+  renderRepositories = () => (
+    this.state.repositories.map(repo => (
+      <Repository key={repo.id} repository={repo} />
+    ))
+  );
+
+  renderList = () => (
+    this.state.repositories.length
+      ? this.renderRepositories()
+      : <Text style={styles.empty}> Nenhum repositório encontrado</Text>
+  );
 
   render() {
     return (
-      <View>
-        <Text>Repos</Text>
+      <View style={styles.container}>
+        { this.state.loading
+        ? <ActivityIndicator size="small" color="#999" />
+        : this.renderList()
+      }
       </View>
     );
   }
---
+++ src/pages/repositories/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import { colors, fonts } from 'styles';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: colors.background,
+  }
+});
+
+export default styles;
